bf850a14be58b1e2ab8d73338811c7c1ed887524
Adair Duarte <ada_dp10@hotmail.com>
2023-08-24 10:17:12 -0600

    WiewBindingIntent
    Se agrego el view binng e intent

---
+++ app/src/main/java/com/example/examen1/ExamenUno.kt
@@ -0,0 +1,16 @@
+package com.example.examen1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.examen1.databinding.ActivityExamenUnoBinding
+
+class ExamenUno : AppCompatActivity() {
+
+    private lateinit var binding: ActivityExamenUnoBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityExamenUnoBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
--- app/src/main/java/com/example/examen1/MainActivity.kt
+++ app/src/main/java/com/example/examen1/MainActivity.kt
@@ -1,11 +1,38 @@
 package com.example.examen1
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.example.examen1.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnComenzar.setOnClickListener {
+            Accion()
+        }
+
     }
+
+    private fun Accion(){
+        if (binding.txtTotal.text.toString().isBlank() || binding.txtCicloUno.text.toString().isBlank() || binding.txtCicloDos.text.toString().isBlank()){
+            Toast.makeText(this, "No pueden estar vacios los campos", Toast.LENGTH_LONG).show()
+        } else{
+            val intent = Intent(this, ExamenUno::class.java)
+            intent.putExtra("Total", binding.txtTotal.text.toString())
+            intent.putExtra("CicloUno", binding.txtCicloUno.text.toString())
+            intent.putExtra("CicloUno", binding.txtCicloDos.text.toString())
+            startActivity(intent)
+        }
+    }
+
+
 }
